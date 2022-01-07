6cb2b93c3ba320dac3cc59128ab638a2f415cd63
Abhishek kochar <43106142+Abhishekkochar@users.noreply.github.com>
2022-01-08 11:11:33 +1300

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -2,10 +2,7 @@ require('babel-polyfill');
 require('babel-register');
 require('dotenv').config();
 const HDWalletProvider = require('@truffle/hdwallet-provider'); //@truffle/hdwallet-provider
-const private_Keys = ['0xc60b9f510739b61e4701519c55676adffed4774b789dd90a3c299372f861c19b',
-'0x493d88132d500d482fb0cffb36a6776dc5dab12dbb9ee4d78a0c33a1afac3c64',
-'0x3f95e21cbeba933b53ed48db72ebe9ccf2622d0a806b8a37bbaeda56d957ec92',
-'0x8578a7dc69b4ff16b1fd6e8c2d6a1d6ecdffbc3abba5b633af51b9d17375e40c'
+const private_Keys = [
 ]
 
 module.exports = {
